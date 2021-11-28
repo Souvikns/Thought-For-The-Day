a0eff8c424aa01828c55b9a4f3938b6c01052087
souvik <souvikde.ns@gmail.com>
2021-11-28 23:50:40 +0530

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,11 +1,11 @@
-const quote = require('inspirational-quotes');
-const textToImage = require('text-to-image');
-const ImageDataURI = require('image-data-uri');
+import quote from 'inspirational-quotes';
+import {generate} from 'text-to-image';
+import ImageDataURI from 'image-data-uri';
 
-module.exports = async (req, res) => {
+export default async (req, res) => {
     res.setHeader('content-type', "image/png");
     res.setHeader('Cace-Control', 'No-Store');
-    const img = await textToImage.generate(quote.getRandomQuote(), {
+    const img = await generate(quote.getRandomQuote(), {
         maxWidth: 800,
         fontSize: 28,
         fontWeight: 'bold',
