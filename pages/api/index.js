8d0f2950930f2de4c055e663fc5a9f7ac8429aa7
souvik <souvikde.ns@gmail.com>
2021-11-29 00:07:19 +0530

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -5,15 +5,14 @@ import ImageDataURI from 'image-data-uri';
 export default async (req, res) => {
     res.setHeader('content-type', "image/png");
     res.setHeader('Cace-Control', 'No-Store');
-    // const img = await generate(quote.getRandomQuote(), {
-    //     maxWidth: 800,
-    //     fontSize: 28,
-    //     fontWeight: 'bold',
-    //     textAlign: 'center',
-    //     textColor: '#11324D',
-    //     margin: 40
-    // });
-    // res.send(ImageDataURI.decode(img).dataBuffer);
-    // return res.end();
-    return res.send("Hello World")
+    const img = await generate(quote.getRandomQuote(), {
+        maxWidth: 800,
+        fontSize: 28,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        textColor: '#11324D',
+        margin: 40
+    });
+    res.send(ImageDataURI.decode(img).dataBuffer);
+    return res.end();
 }
